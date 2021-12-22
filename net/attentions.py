fixed aggregator and incorporated jennys softmax implementation

net/attentions.py
@@ -5,7 +5,7 @@ from torch import nn
 import torch
 import math
 
-from einops import rearrange, repeat
+from einops import rearrange, reduce
 
 
 class MultiHeadDotProduct(nn.Module):
@@ -21,9 +21,12 @@ class MultiHeadDotProduct(nn.Module):
         self.embed_dim = embed_dim
         self.hdim = embed_dim // nhead
         self.nhead = nhead
-        self.aggr = aggr
         self.mult_attr = mult_attr
         self.determinstic = determinstic
+
+        aggr = "sum" if aggr == "add" else aggr
+        assert aggr in ["sum", "max", "mean", "prod"]
+        self.aggr = aggr
 
         # FC Layers for input
         self.q_linear = nn.Linear(embed_dim, embed_dim)
@@ -67,20 +70,16 @@ class MultiHeadDotProduct(nn.Module):
         sim = rearrange(sim, "h n a-> h (n a)")
 
         # Attention scores
-        attn = deterministic_softmax(sim, c, bs).unsqueeze(-1)
+        attn = deterministic_softmax(sim, c, bs)
         # Dropout
         attn = self.dropout(attn)
 
         # Obtain feature matrix and aggregate accordingly
-        feats = attn * v
+        feats = attn.unsqueeze(-1) * v
         feats = rearrange(feats, "h (n a) d-> h n a d", h=self.nhead, n=bs)
-        if self.aggr == "add":
-            feats = torch.sum(feats, dim=-3)
-        elif self.aggr == "max":
-            feats = torch.max(feats, dim=-3).values
-        elif self.aggr == "mean":
-            feats = torch.mean(feats, dim=-3)
 
+        # Aggregate over each node according to aggregator
+        feats = reduce(feats, "h n a d-> h a d", self.aggr)
         feats = rearrange(feats, "h n d -> n (h d)")
 
         # Linear layer

net/gnn_base.py
@@ -179,7 +179,7 @@ class DotAttentionLayer(nn.Module):
         self.res1 = params['res1']
         self.res2 = params['res2']
 
-        self.att = MultiHeadDotProduct(embed_dim, num_heads, aggr, True, 
+        self.att = MultiHeadDotProduct(embed_dim, num_heads, params['aggregator'], True, 
                                         mult_attr=params['mult_attr']).to(dev)
         
         d_hid = 4 * embed_dim if d_hid is None else d_hid

net/utils.py
@@ -1,3 +1,4 @@
+from einops.einops import rearrange
 from torch import nn
 import torch
 from torch_scatter import scatter_max, scatter_add
@@ -175,10 +176,18 @@ def softmax(src, index, dim, dim_size, margin: float = 0.):
 
     return out
 
-def deterministic_softmax(src, index, dim_size, margin: float = 0.0):
+def deterministic_softmax(src, index, batch_size):
+    """
+    Deterministic softmax implementation for nodeswise softmax on all neighbours
+    Variables here:
+        h = nheads
+        n = batch_size = nodes
+        a = adjacent nodes
+    """
+
     # Calculate max per node
-    src2 = rearrange(src, "h (a d)-> h a d", d=dim_size)
-    src_max = torch.clamp(torch.max(src2, dim=-2).values, min=0.0)
+    src2 = rearrange(src, "h (n a)-> h n a", n=batch_size)
+    src_max = torch.max(src2, dim=-2).values
 
     # exp(src - max_node)
     head_indices = torch.arange(src.shape[0]).type(torch.cuda.LongTensor).view(src.shape[0], 1).expand(-1, index.shape[0])
@@ -186,9 +195,9 @@ def deterministic_softmax(src, index, dim_size, margin: float = 0.0):
     src = (src - ext_src_max).exp()
 
     # Calculate denominator
-    src2 = rearrange(src.squeeze(), "h (a d)-> h a d", d=dim_size)
+    src2 = rearrange(src, "h (n a)-> h n a", n=batch_size)
     denom = torch.sum(src2, dim=-2)
-    denom = (denom + (margin - src_max).exp())[head_indices, index]
+    denom = (denom + 1e-16)[head_indices, index]
 
     out = src / denom
 